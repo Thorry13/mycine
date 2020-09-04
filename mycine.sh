02da3853d156f3163c623fb38d537a55c1aee6a0
Remy Dumas <remy.dumas@polymtl.ca>
2020-09-03 23:42:54 -0400

    added summary flag

--- mycine.sh
+++ mycine.sh
@@ -10,6 +10,7 @@ case $1 in
                     python3 -c "from lib import add_movie;\
                                 add_movie('$2')"
             fi;;
+
     -d | --delete)
             if [ $# -eq 4 ] && [ $3 = --dir ]
             then
@@ -19,6 +20,7 @@ case $1 in
                     python3 -c "from lib import delete_movie;\
                                 delete_movie('$2')"
             fi;;
+
     -i | --info)
             if [ $# -eq 4 ] && [ $3 = --dir ]
             then
@@ -28,6 +30,7 @@ case $1 in
                     python3 -c "from lib import display_info;\
                                 display_info('$2')"
             fi;;
+
     -l | --list)
             lmax=25
             watched=None
@@ -48,6 +51,37 @@ case $1 in
             done
             python3 -c "from lib import list_movies;\
                         list_movies(watched=$watched, lmax=$lmax)";;
+
+    -s | --summary)
+            shift
+            lmax=25
+            director=
+            options=0
+            while [ $# -ge 1 ]
+            do
+                case $1 in
+                    --lmax)
+                        lmax=$2
+                        shift; shift;;
+                    --dir)
+                        director=$2
+                        options=1
+                        shift; shift;;
+                esac
+            done
+            if [ $options -eq 1 ]
+            then
+                python3 -c "from lib import make_request;\
+                                        make_request('select title, watched from movies where directors like \'%$director%\'', lmax=$lmax)"
+            else
+                python3 -c "from lib import make_request;\
+                            make_request('select watched, count(*) from movies group by watched', lmax=$lmax)"
+                total=`mycine -l | wc -l`
+                let "total = $total-2" # Remove headlines
+                echo
+                echo "Total : $total"
+            fi;;
+
     -r | --request)
             shift
             lmax=25
@@ -64,6 +98,7 @@ case $1 in
             done
             python3 -c "from lib import make_request;\
                         make_request('$request', lmax=$lmax)";;
+
     -w | --watched)
             dir=None
             watched=1
